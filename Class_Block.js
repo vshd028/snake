51ca88cc74283c1f4cf93edda40cb2f77cad4d06
vshd028 <dragon2008kr@gmail.com>
2022-06-19 11:15:59 +0300

    Class Block changed
    Виправив class Block на методи

--- Class_Block.js
+++ Class_Block.js
@@ -1,50 +1,26 @@
-var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d");
-
-	var width = canvas.width;
-	var height = canvas.height;
-
-	var isGameOver = false
-
-	var colors = ["Blue", "Yellow"]
-
-	var blockSize = 50;
-	var widthInBlocks = width / blockSize;
-	var heightInBlocks = height / blockSize;
-
-	var score = 0;
 
 class Block {
     constructor(col, row) {
         this.col = col
         this.row = row
     }
-}
 
-class DrawSquare extends Block {
-    constructor(color) {
-        super(col, row)
+    drawSquare() {
         let x = this.col * blockSize;
 		let y = this.row * blockSize;
-		ctx.fillStyle = color;
+		ctx.fillStyle = "Green";
 		ctx.fillRect(x, y, blockSize, blockSize);
     }
-}
 
-class DrawCircle extends Block {
-    constructor(color) {
-        super(col, row)
+    drawCircle() {
         let centerX = this.col * blockSize + blockSize / 2;
 		let centerY = this.row * blockSize + blockSize / 2;
-		ctx.fillStyle = color;
+		ctx.fillStyle = "Yellow";
 		circle(centerX, centerY, blockSize / 2, true);
+    }
+
+    equal(otherBlock) {
+        return this.col === otherBlock.col && this.row === otherBlock.row;
     }
 }
 
-class Equal extends Block {
-    constructor(otherBlock) {
-        super(col, row)
-        this.col === otherBlock.col && this.row === otherBlock.row;
-    }
-    
-}
